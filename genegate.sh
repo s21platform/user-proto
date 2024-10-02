908bc78c619d12990c8ef8a878053dc919fcd159
asepopov <asepopov@avito.ru>
2024-10-02 16:46:14 +0300

    add message for kafka

--- genegate.sh
+++ genegate.sh
@@ -3,3 +3,7 @@ protoc  --go_out=./  \
         user.proto
 
 protoc --doc_out=. --doc_opt=markdown,README.md ./user.proto
+
+protoc  --go_out=./  \
+        --go-grpc_out=./ \
+        new_user.proto
